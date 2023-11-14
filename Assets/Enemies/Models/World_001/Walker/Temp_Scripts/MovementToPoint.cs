fe08c6e079d9feace4b8788ba17cb202bd9ec15e
AzureAD\AlejandroLopezHuesca <alejandro.lopez@enti.cat>
2023-11-14 19:21:27 +0100

    Generando un script temporal con el que los enemigos se mueven por la Nav Mesh hasta el punto indicado

---
+++ Assets/Enemies/Models/World_001/Walker/Temp_Scripts/MovementToPoint.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MovementToPoint : MonoBehaviour
+{
+    private Vector3 targetPosition;
+    private NavMeshAgent agent;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        targetPosition = GameObject.FindGameObjectWithTag("Finish").transform.position;
+    }
+
+    private void Start()
+    {
+        agent.SetDestination(targetPosition);
+    }
+
+    private void Update()
+    {
+        if (Vector3.Distance(agent.transform.position, targetPosition) < 0.5f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
