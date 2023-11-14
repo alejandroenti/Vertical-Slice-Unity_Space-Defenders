682a39548698eaff31d588d9d76c047431c5f9aa
AzureAD\AlejandroLopezHuesca <alejandro.lopez@enti.cat>
2023-11-14 18:51:14 +0100

    Añadiendo variable para saber cuantos enemigos deben spawnear

--- Assets/Managers/Spawn_Manager/Spawn_Manager.cs
+++ Assets/Managers/Spawn_Manager/Spawn_Manager.cs
@@ -12,11 +12,28 @@ public class Spawn_Manager : MonoBehaviour
     [SerializeField] private List<GameObject> enemiesToSpawn = new List<GameObject>();
     [SerializeField] private Transform spawnPosition;
 
+    // private int roundEnemies = 0;
+    public int roundEnemies = 0;
     private float spawnTimer = 0.0f;
-    private float spawnTime = 5.0f;
+    private float spawnTime = 0.75f;
 
-    public void SpawnEnemy(enemyClasses enemyClass)
+    private void Update()
     {
-        Instantiate(enemiesToSpawn[((int)enemyClass)], spawnPosition.position, Quaternion.identity);
+        spawnTimer += Time.deltaTime;
+
+        if (spawnTimer >= spawnTime && roundEnemies > 0)
+        {
+            SpawnEnemy(enemyClasses.BASIC);
+        }
+    }
+
+    public void SetRoundEnemies(int roundEnemies) => this.roundEnemies = roundEnemies;
+
+    private void SpawnEnemy(enemyClasses enemyClass)
+    {
+        Instantiate(enemiesToSpawn[(int)enemyClass], spawnPosition.position, Quaternion.identity);
+        
+        spawnTimer = 0.0f;
+        roundEnemies--;
     }
 }
