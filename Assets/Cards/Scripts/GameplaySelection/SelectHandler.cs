16645e0ca4e8c5b50e357ccbb8762704679f0565
Alejandro López <alexxlh.25@gmail.com>
2023-12-09 17:42:25 +0100

    Animación al pasar por encima de la carta en la escena de gameplay

---
+++ Assets/Cards/Scripts/GameplaySelection/SelectHandler.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SelectHandler : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, ISelectHandler, IDeselectHandler
+{
+    [Header("Animation Settings")]
+    [SerializeField] private float verticalAmount = 25f;
+    [SerializeField] private float moveTime = 0.1f;
+    [SerializeField, Range(1f, 2f)] private float scaleAmount = 1.1f;
+
+    // Variables que guardan la posición y la escala inicial
+    private Vector3 initialPosition;
+    private Vector3 initialScale;
+
+    // Variables que guardaran la posición y la escala final
+    private Vector3 finalPosition;
+    private Vector3 finalScale;
+
+    private bool isAnimating = false;
+    private bool animationStarting = false;
+    float animationTimer = 0f;
+
+    private void Awake()
+    {
+        initialPosition = transform.position;
+        initialScale = transform.localScale;
+    }
+
+    private void Update()
+    {
+        if (isAnimating)
+        {
+            ExecuteAnimation();
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        eventData.selectedObject = gameObject;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        eventData.selectedObject = null;
+    }
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        //StartCoroutine(MoveCard(true));
+        StartAnimation(true);
+    }
+
+    public void OnDeselect(BaseEventData eventData)
+    {
+        //StartCoroutine(MoveCard(false));
+        StartAnimation(false);
+    }
+
+    private void StartAnimation(bool isStartingAnimation)
+    {
+        isAnimating = true;
+        animationStarting = isStartingAnimation;
+
+        if (animationStarting)
+        {
+            finalPosition = initialPosition + new Vector3(0f, verticalAmount, 0f);
+            finalScale = initialScale * scaleAmount;
+        }
+        else
+        {
+            finalPosition = initialPosition;
+            finalScale = initialScale;
+        }
+    }
+
+    private void ExecuteAnimation()
+    {
+        animationTimer = Time.deltaTime;
+
+        Vector3 lerpPosition = Vector3.Lerp(transform.position, finalPosition, (animationTimer / moveTime));
+        Vector3 lerpScale = Vector3.Lerp(transform.localScale, finalScale, (animationTimer / moveTime));
+
+        transform.position = lerpPosition;
+        transform.localScale = lerpScale;
+
+        if (animationTimer >= moveTime)
+        {
+            isAnimating = false;
+            animationTimer = 0f;
+        }
+    }
+}
